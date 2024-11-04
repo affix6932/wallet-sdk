464efb78be4a02c2cf7f5dd0e4e23892130ece55
aaaa <aaaa>
2024-11-04 17:57:58 +0800

    add webhook model

---
+++ webhook/model.go
@@ -0,0 +1,28 @@
+package webhook
+
+import (
+	"github.com/shopspring/decimal"
+)
+
+// DepositCallbackMsg deposit callback
+type DepositCallbackMsg struct {
+	Chain   string          `json:"chain" `
+	Hash    string          `json:"hash" `
+	Address string          `json:"address" `
+	Coin    string          `json:"coin" `
+	Amount  decimal.Decimal `json:"amount" `
+	Tag     string          `json:"tag" `
+}
+
+// WithdrawCallbackMsg callback
+type WithdrawCallbackMsg struct {
+	Chain     string          `json:"chain"`
+	Coin      string          `json:"coin"`
+	From      string          `json:"from"`
+	To        string          `json:"to"`
+	Amount    decimal.Decimal `json:"amount"`
+	Tag       string          `json:"tag" `
+	RequestID string          `json:"requestId"`
+	Hash      string          `json:"hash"`
+	Fee       decimal.Decimal `json:"fee"`
+}
